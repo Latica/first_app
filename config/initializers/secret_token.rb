5ec4d06b4655ef4eacb57d550abcdc25154f49b2
latica <laticaitm@gmail.com>
2013-12-02 15:49:52 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '91f2cc4dfe0a7db08bf8bb28757127f0b4e9ed8368d3580060b25ee75422acda48a88391afec99a986a7f69ade2a965abb048bdf2111510599d568371d59e433'
